17c5ef5354a70a8fa18626f0b82bad790f51031f
agmohammed <agmohammed1@gmail.com>
2018-01-30 16:30:35 +0530

    Form 2 updated

--- resources/views/Form2.blade.php
+++ resources/views/Form2.blade.php
@@ -27,88 +27,6 @@
                 <!-- /.box-header -->
                 <div style="background-color:white" class="col-md-6">
                     <div class="box-header with-border">
-                        <h3 class="box-title">User Details</h3>
-                    </div>
-                    <!-- general form elements -->
-
-                    <!-- form start -->
-
-                    <div class="form-horizontal">
-                        <div class="box-body">
-                            <div class="form-group">
-                                <label for="lblFirstName" class="col-sm-2 control-label">First Name</label>
-
-                                <div class="col-sm-10">
-                                    <input type="Text" class="form-control" id="firstName" placeholder="First Name">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="lblLastName" class="col-sm-2 control-label">Last Name</label>
-
-                                <div class="col-sm-10">
-                                    <input type="Text" class="form-control" id="lastName" placeholder="Last Name">
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="lblContactNo" class="col-sm-2 control-label">Contact Number</label>
-
-                                <div class="col-sm-10">
-                                    <input type="Text" class="form-control" id="contactNo" placeholder="Contact Number">
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="lblEmail" class="col-sm-2 control-label">Email</label>
-
-                                <div class="col-sm-10">
-                                    <input type="email" class="form-control" id="email" placeholder=" User Email">
-                                </div>
-                            </div>
-
-
-
-
-
-                            <div class="box box-primary">
-                                <div class="box-header with-border">
-                                    <h3 class="box-title">Add Login Details</h3>
-                                </div>
-                                <!-- /.box-header -->
-                                <div style="background-color:white" class="col-md-6">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="lblCmpnyUsername" class="col-sm-2 control-label">Username</label>
-
-                                <div class="col-sm-10">
-                                    <input type="Text" class="form-control" id="cmpnyUsername" placeholder="Username">
-
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="lblCmpnyPW" class="col-sm-2 control-label">Password</label>
-
-                                <div class="col-sm-10">
-                                    <input type="Password" class="form-control" id="cmpnyPW" placeholder="Password">
-
-                                </div>
-                            </div>
-
-
-
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-
-
-                <div style="background-color:white" class="col-md-6">
-                    <div class="box-header with-border">
                         <h3 class="box-title">Company Details</h3>
                     </div>
                     <!-- general form elements -->
@@ -117,6 +35,13 @@
 
                     <div class="form-horizontal">
                         <div class="box-body">
+
+
+
+                            
+                            
+
+
                             <div class="form-group">
                                 <label for="lblCompanyName" class="col-sm-2 control-label">Company Name</label>
 
@@ -149,35 +74,60 @@
                                 </div>
                             </div>
 
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+                <div style="background-color:white" class="col-md-6">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Add Login Details</h3>
+                    </div>
+                    <!-- general form elements -->
+
+                    <!-- form start -->
+
+                    <div class="form-horizontal">
+                        <div class="box-body">
                             <div class="form-group">
-                                <label for="lblCmpnyCity" class="col-sm-2 control-label">City</label>
+                                <label for="lblCmpnyUsername" class="col-sm-2 control-label">Username</label>
 
                                 <div class="col-sm-10">
-                                    <input type="Text" class="form-control" id="cmpnyCity" placeholder="Company City">
+                                    <input type="Text" class="form-control" id="cmpnyUsername" placeholder="Username">
 
                                 </div>
                             </div>
 
                             <div class="form-group">
-                                <label for="lblCmpnyCountry" class="col-sm-2 control-label">Country</label>
+                                <label for="lblCmpnyPW" class="col-sm-2 control-label">Password</label>
 
                                 <div class="col-sm-10">
-                                    <input type="Text" class="form-control" id="cmpnyCountry" placeholder="Company Country">
+                                    <input type="Password" class="form-control" id="cmpnyPW" placeholder="Password">
 
                                 </div>
                             </div>
-
-
-
 
                         </div>
                         <!-- /.box-body -->
-                        <div class="box-footer">
-                            <button class="btn btn-default">Cancel</button>
-                            <button type="submit" class="btn btn-info pull-right">Submit</button>
-                        </div>
-                        <!-- /.box-footer -->
+                        
+
                     </div>
+
+                    <div style="background-color:white">
+
+                        <div style="float:right; padding-left:2%">
+                          <button type="submit" class="btn btn-info pull-right">Submit</button>
+                        </div>
+
+                        <div style="float:right">
+                          <button class="btn btn-default">Cancel</button>
+                        </div>
+                      </div>
 
                 </div>
 
