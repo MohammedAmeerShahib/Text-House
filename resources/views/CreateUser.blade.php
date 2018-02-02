94aaba35ad2e7a9b30bcf0f96e7115f6e9265706
agmohammed <agmohammed1@gmail.com>
2018-02-02 15:37:38 +0530

    Updated CreateUser

--- resources/views/CreateUser.blade.php
+++ resources/views/CreateUser.blade.php
@@ -3,7 +3,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            Bulk Message
+
         </h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
@@ -18,20 +18,19 @@
 
 
 
-        <div class="col-sm-2" style="margin-top: 4%;margin-bottom: 2%">
-            <button type="button" class="btn btn-block btn-success" data-toggle="modal" data-target=".bs-modal-sm">
-                <i class="glyphicon glyphicon-plus"></i>
-                <b>&nbsp;&nbsp;CREATE</b>
-            </button>
+        {{--<div class="col-sm-2" style="margin-top: 4%;margin-bottom: 2%">--}}
+            {{--<button type="button" class="btn btn-block btn-success" data-toggle="modal" data-target=".bs-modal-sm">--}}
+                {{--<i class="glyphicon glyphicon-plus"></i>--}}
+                {{--<b>&nbsp;&nbsp;CREATE</b>--}}
+            {{--</utton>--}}
 
-        </div>
+        {{--</div>--}}
         <table id="SMS" class="table table-bordered table-hover dataTable" role="grid">
             <thead>
             <tr role="row">
-                <th width="7%">Number</th>
-                <th width="15%">Created At</th>
-                <th width="15%">Last Updated</th>
-                <th width="10%">List size </th>
+                <th width="3%">#</th>
+                <th width="20%">Enterprise Name</th>
+                <th width="20%">Enterprise Email</th>
                 <th width="20%">Action</th>
             </tr>
             </thead>
@@ -41,10 +40,8 @@
                 <td>1</td>
                 <td>02:04 04/01/2018</td>
                 <td>Shipa</td>
-                <td>This is to inform you that we have</td>
-                <td><a class="btn btn-sm btn-success" href="javascript:void(0)" title="Hapus" ><i class="glyphicon glyphicon-envelope"></i> View  </a>
-                    <a class="btn btn-sm btn-primary" href="javascript:void(0)" title="Edit" ><i class="glyphicon glyphicon-pencil"></i> Edit  </a>
-                    <a class="btn btn-sm btn-danger" href="javascript:void(0)" title="Hapus" ><i class="glyphicon glyphicon-trash"></i> Delete  </a>
+                <td><a class="btn btn-sm btn-primary" href="javascript:void(0)" title="Edit" data-toggle="modal" data-target=".bs-modal-sm"><i class="glyphicon glyphicon-user"></i> Create Account  </a>
+
                 </td>
             </tr>
             <tr>
@@ -75,7 +72,7 @@
         </table>
 
 
-        <?php   include(app_path().'/Includes/CreateList.php'); ?>
+        <?php   include(app_path().'/Includes/EntCreateList.php'); ?>
 
     </section>
 
